7b1d66777e45fbfe0eac9cb290332f7e33d50f2b
Seb <bones@ucc.asn.au>
2025-02-03 00:23:28 +0800

    lessBrokenSysDTake6

--- pkgs/fanControl/default.nix
+++ pkgs/fanControl/default.nix
@@ -3,20 +3,20 @@
 
 let fanControl = pkgs.callPackage ./package.nix { inherit pkgs lib; };
 in rec {
-
   environment.systemPackages = [ fanControl ];
-  systemd.services."fanControl" = {
-    enable = true;
-    after = ["network.target"];
-    description = "Fan Speed Service";
-    serviceConfig = {
-      Type="simple";
-      ExecStart="fanControl";
-      Restart="on-failure";
-      RestartSec="1s";
-      SyslogIdentifier="fanControl";
-      User="root";
-      Group="root";
-    };
-  };
+
+  # systemd.services."fanControl" = {
+  #   enable = true;
+  #   after = ["network.target"];
+  #   description = "Fan Speed Service";
+  #   serviceConfig = {
+  #     Type="simple";
+  #     ExecStart="fanControl";
+  #     Restart="on-failure";
+  #     RestartSec="1s";
+  #     SyslogIdentifier="fanControl";
+  #     User="root";
+  #     Group="root";
+  #   };
+  # };
 }
