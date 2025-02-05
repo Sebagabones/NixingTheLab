de69c45dc3b6834396edd6061e1ff033f4a2f3d7
Seb <bones@ucc.asn.au>
2025-02-05 22:17:08 +0800

    urgh

--- configuration.nix
+++ configuration.nix
@@ -37,6 +37,7 @@
   };
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.trusted-users = ["root" "@wheel"];
 
   # Networking
   networking.networkmanager.enable = true;
