583d4e3244bbd03dd86af90f9b04da893aea332a
Seb <bones@ucc.asn.au>
2025-02-09 17:39:43 +0800

    Whoops, probs need a bootable partition huh

--- diskConfig.nix
+++ diskConfig.nix
@@ -7,11 +7,11 @@
         content = {
           type = "gpt";
           partitions = {
-            # MBR = {
-            #   type = "EF02"; # for grub MBR
-            #   size = "1M";
-            #   priority = 1; # Needs to be first partition
-            # };
+            MBR = {
+              type = "EF02"; # for grub MBR
+              size = "1M";
+              priority = 1; # Needs to be first partition
+            };
             ESP = {
               type = "EF00";
               size = "1G";
