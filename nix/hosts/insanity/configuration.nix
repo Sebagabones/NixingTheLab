a41614dd62488bb7598d672de2901eafc2af0235
Seb <bones@ucc.asn.au>
2025-06-21 16:07:15 +0800

    sway ish pt 2

--- nix/hosts/insanity/configuration.nix
+++ nix/hosts/insanity/configuration.nix
@@ -14,8 +14,10 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelModules = [ "kvm_intel" ];
 
   programs.dconf.enable = true;
+
   # Packages
   environment.systemPackages = with pkgs; [
     networkmanagerapplet
@@ -31,33 +33,59 @@
 
   environment.pathsToLink =
     [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw
+  security.rtkit.enable = true;
+  security.polkit.enable = true;
+  services.dbus.enable = true;
+
+  services.pulseaudio.enable = false;
+  services.pipewire = {
+    enable = true;
+    audio.enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+  };
 
   services.xserver = {
     enable = true;
-
+    videoDrivers = [ "amd" ];
     desktopManager = { xterm.enable = false; };
 
-    displayManager = {
-      lightdm = {
-        enable = true;
+    # displayManager = {
+    #   lightdm = {
+    #     enable = true;
 
-        greeters.slick = { enable = true; };
-        background = ./backgroundFR.png;
+    #     greeters.slick = {
+    #       enable = true;
+    #       extraConfig = ''
+    #         enable-hidpi=off
+    #         xft-dpi=81.59'';
+    #     };
+    #     background = ./backgroundFR.png;
+    #   };
+    # };
+
+    #   windowManager.i3 = {
+    #     enable = true;
+    #     extraPackages = with pkgs; [
+    #       dmenu # application launcher most people use
+    #       i3status # gives you the default i3 status bar
+    #       i3lock # default i3 screen locker
+    #       i3blocks # if you are planning on using i3blocks over i3status
+    #     ];
+    #   };
+  };
+  # services.displayManager.defaultSession = "none+i3";
+  services.libinput.enable = true;
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
+        user = "greeter";
       };
     };
-
-    windowManager.i3 = {
-      enable = true;
-      extraPackages = with pkgs; [
-        dmenu # application launcher most people use
-        i3status # gives you the default i3 status bar
-        i3lock # default i3 screen locker
-        i3blocks # if you are planning on using i3blocks over i3status
-      ];
-    };
   };
-  services.displayManager.defaultSession = "none+i3";
-
+  # services.displayManager.defaultSession = "sway";
   # Networking
   networking.networkmanager.enable = true;
   networking.networkmanager.wifi.powersave = true;
--- nix/modules/home/sway.nix
+++ nix/modules/home/sway.nix
@@ -1,43 +1,10 @@
 { pkgs, config, lib, perSystem, ... }:
 
 let
-  wmenu = with config.lib.stylix.colors;
-    pkgs.writeShellScriptBin "wmenu" ''
-      ${pkgs.wmenu}/bin/wmenu -N ${base00} -M ${base01} -S ${base02} -n ${base05} -m ${base05} -s ${base0D} -f "monospace 13" -b "$@"
-    '';
-  dmenu = pkgs.stdenv.mkDerivation {
-    name = "dmenu";
-    buildCommand = ''
-      mkdir -p $out/bin
-      ln -s ${lib.getExe wmenu} $out/bin/dmenu
-    '';
-    meta.mainProgram = "dmenu";
-  };
-  menu-d = pkgs.writeShellScriptBin "menu-d.sh" ''
-    sock="$XDG_RUNTIME_DIR/j4dmenu"
-    if pgrep j4-dmenu-deskto >/dev/null; then
-      echo q > "$sock"
-      rm -f "$sock"
-      while pgrep j4-dmenu-deskto; do
-        sleep 0.1
-        pkill -QUIT j4-dmenu-deskto
-      done
-    fi
-    ${pkgs.j4-dmenu-desktop}/bin/j4-dmenu-desktop \
-      --i3-ipc \
-      --dmenu "${lib.getExe wmenu} -i" \
-      --term "${terminal}" \
-      --usage-log ~/.cache/j4dmenu \
-      --no-generic \
-      --use-xdg-de \
-      --wait-on "$sock"
-  '';
-  terminal = pkgs.writeShellScript "terminal.sh" ''
-    ${lib.getExe config.programs.ghostty.package}
-  '';
+  terminal = "wezterm";
   modifier = "Mod4"; # Super
 in {
-  home.packages = [ wmenu dmenu menu-d pkgs.gtklock ];
+  home.packages = [ pkgs.bemenu pkgs.wezterm ];
 
   wayland.windowManager.sway = {
     enable = true;
@@ -56,25 +23,7 @@ in {
       inherit modifier;
       terminal = toString terminal;
       defaultWorkspace = "workspace number 1";
-      menu = ''pkill -QUIT wmenu || echo > "$XDG_RUNTIME_DIR/j4dmenu"'';
-      startup = [
-        {
-          always = true;
-          command = lib.getExe menu-d;
-        }
-        {
-          always = true;
-          command = lib.getExe pkgs.albert;
-        }
-        {
-          always = true;
-          command = "pgrep solaar || solaar --window hide";
-        }
-        {
-          always = true;
-          command = "kanshictl reload";
-        }
-      ];
+
       window = {
         border = 0;
         titlebar = false;
@@ -96,45 +45,6 @@ in {
         "type:keyboard" = { xkb_options = "caps:swapescape"; };
         "1:1:solaar-keyboard" = { scroll_factor = "0.04"; };
       };
-      bars = [{
-        statusCommand =
-          "${pkgs.i3status-rust}/bin/i3status-rs config-bottom.toml";
-        trayOutput = "none";
-        fonts = {
-          names = [ "monospace" ];
-          size = 10.0;
-        };
-        colors = with config.lib.stylix.colors.withHashtag; {
-          background = base00;
-          separator = base01;
-          statusline = base04;
-          focusedWorkspace = {
-            border = base02;
-            background = base02;
-            text = base0D;
-          };
-          activeWorkspace = {
-            border = base00;
-            background = base00;
-            text = base0D;
-          };
-          inactiveWorkspace = {
-            border = base00;
-            background = base00;
-            text = base05;
-          };
-          urgentWorkspace = {
-            border = base00;
-            background = base00;
-            text = base09;
-          };
-          bindingMode = {
-            border = base00;
-            background = base00;
-            text = base09;
-          };
-        };
-      }];
       keybindings = let
         exec = args: "exec ${lib.getExe (pkgs.writeShellApplication args)}";
         swayosd = cmd:
@@ -150,23 +60,21 @@ in {
             text = "playerctl ${cmd}";
           };
       in lib.mkOptionDefault {
-        "${modifier}+alt+l" =
-          "exec ${lib.getExe pkgs.gtklock} -d && systemctl suspend";
-        "${modifier}+alt+shift+l" = "exec ${lib.getExe pkgs.gtklock} -d";
+        "${modifier}+shift+Right" = "move right";
+        "${modifier}+shift+l" = "exec ${lib.getExe pkgs.swaylock} -d";
         "${modifier}+alt+v" = exec {
           name = "clipboard-picker";
-          runtimeInputs = [ pkgs.cliphist wmenu ];
+          runtimeInputs = [ pkgs.cliphist pkgs.wmenu ];
           text =
             "cliphist list | wmenu -l5 -pclipboard -i | cliphist decode | wl-copy";
         };
         "${modifier}+alt+shift+v" = exec {
           name = "clipboard-delete-picker";
-          runtimeInputs = [ pkgs.cliphist wmenu ];
+          runtimeInputs = [ pkgs.cliphist pkgs.wmenu ];
           text = ''
             cliphist list | wmenu -l5 -p"clipboard delete" -i | cliphist-delete'';
         };
-        "${modifier}+alt+w" = "exec ${lib.getExe pkgs.networkmanager_dmenu}";
-        "${modifier}+space" = "exec ${lib.getExe pkgs.albert} toggle";
+        "${modifier}+w" = "exec ${lib.getExe pkgs.firefox}";
 
         XF86AudioRaiseVolume = swayosd "output-volume raise";
         XF86AudioLowerVolume = swayosd "output-volume lower";
