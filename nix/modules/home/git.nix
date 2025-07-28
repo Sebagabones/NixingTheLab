b7ffea566d28db6982cb5b47be8c0b47a91bed2f
Sebgabones <you@example.com>
2025-07-28 16:08:34 +0800

    added gpg and git signing

--- nix/modules/home/git.nix
+++ nix/modules/home/git.nix
@@ -7,8 +7,11 @@
     userName = "Sebgabones";
     userEmail = "133339614+Sebagabones@users.noreply.github.com";
     extraConfig = {
+      user = { signingkey = "~/.ssh/id_ed25519.pub"; };
       push = { autoSetupRemote = true; };
       pull = { rebase = false; };
+      gpg = { format = "ssh"; };
+      commit = { gpgsign = true; };
     };
     # delta = {
     #   enable = true;
