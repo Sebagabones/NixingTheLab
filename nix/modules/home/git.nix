b05bff8c1528ba5d9895f9c4bc854c68595a4d91
Sebgabones <133339614+Sebagabones@users.noreply.github.com>
2025-08-02 21:16:36 +0800

    delta setup for magit-delta

--- nix/modules/home/git.nix
+++ nix/modules/home/git.nix
@@ -12,13 +12,30 @@
       pull = { rebase = false; };
       commit = { gpgsign = true; };
       gpg = { format = "ssh"; };
+      delta = {
+        features = "side-by-side";
+        syntax-theme = "tokyoNightNight";
+        minus-style = "syntax '#37222c'";
+        minus-non-emph-style = "syntax '#37222c'";
+        minus-emph-style = "syntax '#713137'";
+        minus-empty-line-marker-style = ''syntax "#37222c"'';
+        line-numbers-minus-style = "#914c54";
+        plus-style = ''syntax "#20303b"'';
+        plus-non-emph-style = ''syntax "#20303b"'';
+        plus-emph-style = ''syntax "#2c5a66"'';
+        plus-empty-line-marker-style = ''syntax "#20303b"'';
+        line-numbers-plus-style = "#449dab";
+        line-numbers-zero-style = "#3b4261";
+      };
     };
-    delta = {
-      enable = false;
-      options = { };
-    };
+
+    # delta = {
+    # enable = true;
+    # options = { features = {  }; };
+    # };
     difftastic = {
       enable = true;
+      # enable = false;
       enableAsDifftool = true;
       color = "auto";
       display = "side-by-side";
