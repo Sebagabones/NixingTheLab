b719d50eecfe2686dc85ed363cedb30192554741
Sebgabones <133339614+Sebagabones@users.noreply.github.com>
2025-08-02 23:50:15 +0800

    removed custom delta package

--- nix/modules/home/bones.nix
+++ nix/modules/home/bones.nix
@@ -138,8 +138,9 @@ in {
         command_timeout = 1300;
         scan_timeout = 50;
         # format = ''
-        #   $all$nix_shell$c$$golang$rust$python$git_branch$git_commit$git_state$git_status
-        #   $username$hostname$directory'';
+        #   $all$nix_shell$c$golang$rust$python$nim$git_branch$git_commit$git_state$git_status
+        #   $username$hostname$directory
+        # '';
         character = {
           success_symbol = "[](bold green) ";
           error_symbol = "[✗](bold red) ";
