82339b404f40235fa15a6b9e7c618d5adff59d2b
Sebgabones <133339614+Sebagabones@users.noreply.github.com>
2025-08-10 20:40:35 +0800

    should make sure nix shell is update to date?
    no idea if i actually need to do this but meh

--- nix/modules/nixos/base.nix
+++ nix/modules/nixos/base.nix
@@ -24,6 +24,13 @@
   boot.tmp.tmpfsSize = "50%";
 
   nix = {
+    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+
+    # This will additionally add your inputs to the system's legacy channels
+    # Making legacy nix commands consistent as well, awesome!
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}")
+      config.nix.registry;
+
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
       trusted-users = [ "root" "@wheel" ];
