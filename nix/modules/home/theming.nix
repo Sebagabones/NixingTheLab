d120dcc60169f279de38b510bca10cd16fbcba56
Sebgabones <133339614+Sebagabones@users.noreply.github.com>
2025-08-10 20:41:01 +0800

    more theming
    moonlight did look nice sometimes

--- nix/modules/home/theming.nix
+++ nix/modules/home/theming.nix
@@ -6,7 +6,12 @@
 
     # polarity = "dark";
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/da-one-ocean.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/da-one-ocean.yaml";
+    # base16Scheme =
+    #   "${pkgs.base16-schemes}/share/themes/tokyo-night-terminal-dark.yaml";
+    base16Scheme =
+      "${pkgs.base16-schemes}/share/themes/tokyodark-terminal.yaml";
+
     targets.bat.enable = false;
     # fonts = {
     #
