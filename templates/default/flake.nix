e96439886cd5962d9a34e457e69c2a21f720c351
Sebgabones <133339614+Sebagabones@users.noreply.github.com>
2026-01-18 14:36:48 +0800

    updated template devshell

--- templates/default/flake.nix
+++ templates/default/flake.nix
@@ -1,72 +1,85 @@
-{ pkgs }:
+{
+  description = "Python+Latex devshell";
 
-pkgs.mkShell {
-  nativeBuildInputs = [
-    pkgs.python313.withPackages
-    (ps: [
-      ps.pygments
-      ps.catppuccin
-      ps.webcolors
-      ps.pdoc
-      ps.dot2tex
-      ps.webcolors
-      ps.pygments
-      ps.catppuccin
-      ps.pdoc
-      ps.dot2tex
-      ps.svg2tikz
-      ps.numpy
-    ])
-    (pkgs.texlive.combine {
-      inherit (pkgs.texlive)
-        scheme-basic
-        dvisvgm
-        dvipng # for preview and export as html
-        wrapfig
-        amsmath
-        ulem
-        hyperref
-        capt-of
-        fontspec
-        listings
-        xcolor
-        koma-script
-        multirow
-        lstfiracode
-        fvextra
-        upquote
-        lineno
-        tcolorbox
-        latexmk
-        minted
-        enumitem
-        catppuccinpalette
-        pdfcol
-        caption
-        latex-graphics-dev
-        booktabs
-        framed
-        changepage
-        svg
-        transparent
-        dot2texi
-        moreverb
-        xkeyval
-        graphviz
-        standalone
-        luatex85
-        pdflscape
-        etoc
-        titlesec
-        preview
-        ;
-    })
-    pkgs.latexminted
-    pkgs.inkscape
-    pkgs.pdf2svg
-  ];
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
-  shellHook = ''
-    export PYTHONPATH=$(pwd)
-  '';
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell rec {
+        nativeBuildInputs = [
+          pkgs.python313.withPackages
+          (ps: [
+            ps.pygments
+            ps.catppuccin
+            ps.webcolors
+            ps.pdoc
+            ps.dot2tex
+            ps.webcolors
+            ps.pygments
+            ps.catppuccin
+            ps.pdoc
+            ps.dot2tex
+            ps.svg2tikz
+            ps.numpy
+          ])
+          (pkgs.texlive.combine {
+            inherit (pkgs.texlive)
+              scheme-basic
+              dvisvgm
+              dvipng # for preview and export as html
+              wrapfig
+              amsmath
+              ulem
+              hyperref
+              capt-of
+              fontspec
+              listings
+              xcolor
+              koma-script
+              multirow
+              lstfiracode
+              fvextra
+              upquote
+              lineno
+              tcolorbox
+              latexmk
+              minted
+              enumitem
+              catppuccinpalette
+              pdfcol
+              caption
+              latex-graphics-dev
+              booktabs
+              framed
+              changepage
+              svg
+              transparent
+              dot2texi
+              moreverb
+              xkeyval
+              graphviz
+              standalone
+              luatex85
+              pdflscape
+              etoc
+              titlesec
+              preview
+              ;
+          })
+          pkgs.latexminted
+          pkgs.inkscape
+          pkgs.pdf2svg
+        ];
+
+        shellHook = ''
+          export PYTHONPATH=$(pwd)
+        '';
+      };
+    };
 }
