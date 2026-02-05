d6e2ce10ec900bbbb0efe6b6198529c85785a299
Sebgabones <133339614+Sebagabones@users.noreply.github.com>
2026-02-05 20:59:43 +0800

    virtulisation

--- modules/nixos/podman.nix
+++ modules/nixos/podman.nix
@@ -3,6 +3,14 @@
   # Enable common container config files in /etc/containers
   virtualisation.containers.enable = true;
   virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu.vhostUserPackages = with pkgs; [ virtiofsd ];
+    };
+
+    # Enable USB redirection
+    spiceUSBRedirection.enable = true;
+
     podman = {
       enable = true;
 
@@ -14,6 +22,11 @@
     };
   };
 
+  services.spice-vdagentd.enable = true; # enable copy and paste between host and guest
+  services.qemuGuest.enable = true;
+  networking.firewall.trustedInterfaces = [ "virbr0" ];
+  programs.virt-manager.enable = true;
+
   # Useful other development tools
   environment.systemPackages = with pkgs; [
     dive # look into docker image layers
@@ -21,5 +34,6 @@
     # docker-compose # start group of containers for dev
     podman-compose # start group of containers for dev
     distrobox
+    dnsmasq
   ];
 }
