ec25eb3bfb3737623e3f298ecce1a924516e847c
Sebgabones <133339614+Sebagabones@users.noreply.github.com>
2026-07-26 19:10:07 +0800

    python template

---
+++ templates/python/flake.nix
@@ -0,0 +1,84 @@
+{
+  description = "Python Flake Template"; # TODO:
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems =
+        f:
+        builtins.listToAttrs (
+          map (system: {
+            name = system;
+            value = f system;
+          }) systems
+        );
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          PythonFlakeTemplate = pkgs.python3Packages.buildPythonPackage rec {
+            # TODO:
+            pname = "Python Flake Template"; # TODO:
+            version = "0.0.1"; # TODO:
+            pyproject = true;
+            src = self;
+
+            dependencies = with pkgs.python3Packages; [
+              # TODO:
+              mypy
+              ast-serialize
+            ];
+
+            build-system = with pkgs.python3Packages; [
+              hatch
+              hatch-mypyc
+              hatchling
+            ];
+            meta = {
+              description = "A flake template for Python Projects"; # TODO:
+              maintainers = [ ]; # TODO:
+            };
+          };
+          default = self.packages.${system}.PythonFlakeTemplate;
+        }
+      );
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell rec {
+            nativeBuildInputs = with pkgs; [
+              uv
+              (python314.withPackages (
+                ps: with ps; [
+                  hatch
+                  hatch-mypyc
+                  hatchling
+                ]
+              ))
+              self.packages.${system}.default
+            ];
+
+            shellHook = ''
+              export PYTHONPATH=$(pwd)
+            '';
+          };
+        }
+      );
+    };
+
+}
